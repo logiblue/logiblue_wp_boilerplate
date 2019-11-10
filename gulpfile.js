29448f7af50ac28448f30e69a95d5877e863f10d
logiblue <karanikolasms@gmail.com>
2019-11-10 21:50:40 +0200

    Gulp Intergration

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,45 @@
-//imports
-
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const groupmq = require('gulp-group-css-media-queries');
-const bs = require('browser-sync');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
 
-//Paths
+// Variables
+var paths = {
+    styles: {
+        src: "styles/**/*.scss",
+        dest: "."
+    }
+};
 
-const SASS_SOURCES = [
-  './*.scss', // This picks up our style.scss file at the root of the theme
-  './sass/**/*.scss', // All other Sass files in the /css directory,
-];
+// Define tasks after requiring dependencies
+function style() {
+    return gulp
+        .src(paths.styles.src)
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 
+}
 
+exports.style = style;
 
+function reload() {
+    browserSync.reload();
+}
 
-/**
- * Compile Sass files
- */
-gulp.task('compile:sass', () =>
-  gulp.src(SASS_SOURCES, { base: './' })
-    .pipe(plumber()) // Prevent termination on error
-    .pipe(sass({
-      indentType: 'tab',
-      indentWidth: 1,
-      outputStyle: 'expanded', // Expanded so that our CSS is readable
-    })).on('error', sass.logError)
-    .pipe(postcss([
-      autoprefixer({
-        cascade: false,
-      })
-    ]))
-    .pipe(groupmq()) // Group media queries!
-    .pipe(gulp.dest('.')) // Output compiled files in the same dir as Sass sources
-    .pipe(bs.stream())); // Stream to browserSync
+function watch(){
+    browserSync.init({
+        proxy: "localhost/artware"
+    });
 
-
-
-/**
- * Start up browserSync and watch Sass files for changes 
- */
- 
-gulp.task('watch:sass', ['compile:sass'], () => {
-  bs.init({
-    proxy: 'http://localhost/atw'
-  });
-  gulp.watch(SASS_SOURCES, ['compile:sass']);
-  gulp.watch('**/*.php').on('change', function () { //this watch inspects the php files for changes 
-        bs.reload();
+    gulp.watch(paths.styles.src, style)
+    gulp.watch('**/*.php').on('change', function () { //this watch inspects the php files for changes 
+        browserSync.reload();
   });  
-});
+}
+    
+// Don't forget to expose the task!
+exports.watch = watch
 
-//The main task that we need to run in terminal
-gulp.task('default', ['watch:sass']);
+
