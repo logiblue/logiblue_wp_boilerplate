cb78774bb737631c0a5c2cb1592843006adeaabe
logiblue <hello@karanikolas.work>
2020-04-05 02:09:56 +0300

    last-resets-start-develop

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 	echo('<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">');
 	?>
 
-	<meta name="author" content="Artware">
+	<meta name="author" content="karanikolas">
 	<meta name="theme-color" content="">
 	<meta name="msapplication-TileColor" content="">
 	<meta name="msapplication-navbutton-color" content="">
@@ -25,57 +25,16 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/style.css">
 
-	<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/js/simpleLightbox.min.css">
-	<script src="<?php echo bloginfo('template_url'); ?>/js/simpleLightbox.min.js"></script>
 
-	<?php if ( !is_page() ) { ?>
-	<link rel="stylesheet" id="js_composer_front-css"  href="<?php echo bloginfo('url'); ?>/wp-content/plugins/js_composer/assets/css/js_composer.min.css" type="text/css" media="all">
-	<?php } ?>
 
-  <!--[if lt IE 9]>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <![endif]-->
 
 	<?php wp_head();?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<div href="#0" class="scrollToTop"></div>
 
 <header>
 
-<!--
-<div class="header-top">
-	<div class="header-top-first">
-		<ul>
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		</ul>
-	</div>
-	<div class="header-top-second">
-		<ul>
-			<?php dynamic_sidebar( 'sidebar-2' ); ?>
-		</ul>
-	</div>
-</div>
--->
-
-<div class="header">
-	<div class="header-logo">
-		<a href="<?php bloginfo('url'); ?>"><img src='http://localhost/artware/wp-content/uploads/2019/11/Artware-logo@4x.png' alt="<?php echo bloginfo('name'); ?>" width="" height="" /></a>
-	</div>
-	<div class="header-navigation">
-		<nav>
-			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-		</nav>
-	</div>
-	<div class="header-lang">
-		<p class=delete-test-p>EN / GR</p>
-		<!-- <?php dynamic_sidebar( 'sidebar-7' ); ?> -->
-	</div>
-	<div class="header-btn">
-		GET STARTED
-	</div>
-</div>
 	
 </header>
